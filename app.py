3e9d80c25efc1d23059e1f95a8088273e0ba461a
chipzymango <chipzymango@gmail.com>
2022-03-30 09:37:38 +0200

    Added refresh button

--- app.py
+++ app.py
@@ -1,48 +1,65 @@
 import tkinter as tk
-from win32api import GetSystemMetrics
-
-from get_data import name_of_station, nearest_stop_time, line
+from tkinter import StringVar
+from get_data import name_of_station, nearest_stop_time, line, r, delayed_stop_time
 from datetime import datetime
 
-# get the current time
-current_time = datetime.now()
-
-# convert to string so we can edit it
-current_time = str(current_time)
-current_time = current_time.split(" ", 2)[1]
-current_time = current_time.split(".", 1)[0]
-current_time = current_time.rsplit(":", 1)[0]
-
-
-# get bus arrival time
-shortened_bus_arrival_time = str(nearest_stop_time)
-shortened_bus_arrival_time = shortened_bus_arrival_time.split("T", 1)[1]
-shortened_bus_arrival_time = shortened_bus_arrival_time.split("+", 1)[0]
-shortened_bus_arrival_time = shortened_bus_arrival_time.rsplit(":", 1)[0]
-print(shortened_bus_arrival_time)
-
-print(current_time)
-
-name_of_line = line['line']['name']
-
-# get the time differance in minutes
-time_left = int( str(shortened_bus_arrival_time.replace(":", "")) ) - int( str(current_time.replace(":", "")) )
-
-time_left_message = str(time_left) + " minutes remaining " + "("+shortened_bus_arrival_time+") " + "till " + str(name_of_line)#['id']['name']
-
-
 root = tk.Tk()
-
+remaining_time_msg_str_var = StringVar()
 root.geometry('640x480')
 root.title("Timetables")
+
+def refresh_time():
+	from get_data import name_of_station, nearest_stop_time, line, r
+	nearest_stop_time = r['data']['stopPlace']['estimatedCalls'][0]['aimedArrivalTime']
+	# get the current time
+	current_time = datetime.now()
+
+	# convert to string so we can edit it
+	current_time = str(current_time)
+	current_time = current_time.split(" ", 2)[1]
+	current_time = current_time.split(".", 1)[0]
+	current_time = current_time.rsplit(":", 1)[0]
+
+	shortened_bus_arrival_time = str(nearest_stop_time)
+	shortened_bus_arrival_time = shortened_bus_arrival_time.split("T", 1)[1]
+	shortened_bus_arrival_time = shortened_bus_arrival_time.split("+", 1)[0]
+	shortened_bus_arrival_time = shortened_bus_arrival_time.rsplit(":", 1)[0]
+
+	shortened_bus_arrival_delayed = str(delayed_stop_time)
+	shortened_bus_arrival_delayed = shortened_bus_arrival_delayed.split("T", 1)[1]
+	shortened_bus_arrival_delayed = shortened_bus_arrival_delayed.split("+", 1)[0]
+	shortened_bus_arrival_delayed = shortened_bus_arrival_delayed.rsplit(":", 1)[0]
+	name_of_line = line['line']['name']
+	time_left = int( str(shortened_bus_arrival_time.replace(":", "")) ) - int( str(current_time.replace(":", "")) )
+
+	if time_left < 0:
+		time_left_message = '[{0}]'.format(str(name_of_line)) + " The bus is late."
+	elif time_left == 0:
+		time_left_message = '[{0}]'.format(str(name_of_line)) + " The bus is arriving now."
+	else:
+		time_left_message = '[{0}]'.format(str(name_of_line)) + " ca. " + str(time_left) + " minutes remaining."
+
+	print(nearest_stop_time)
+	print("Timetable updated: " + str(time_left_message))
+	print("Delayed time: " + str(shortened_bus_arrival_delayed))
+
+	# update time left message
+	remaining_time_msg_str_var.set(time_left_message)
+
+	return remaining_time_msg_str_var
+
+remaining_time_display_msg = refresh_time()
 
 bg_frame = tk.Frame(root, bg="#ffeeee")
 bg_frame.place(relx=0, rely=0, relwidth=1, relheight=1)
 
-label1 = tk.Label(bg_frame, bg="#ffffff", text=name_of_station, font=25)
-label1.place(relx= 0.5-0.35, rely=0.4-0.25, relwidth=0.7, relheight=0.25)
+station_name_label = tk.Label(bg_frame, bg="#ffffff", text=name_of_station, font=100)
+station_name_label.place(relx= 0.5-0.35, rely=0.4-0.25, relwidth=0.7, relheight=0.25)
 
-label2 = tk.Label(bg_frame, bg="#ffffff", text=time_left_message)#,# font=10)
-label2.place(relx=0.5-0.35, rely=0.7, relwidth=0.7, relheight=0.2)
+display_remaining_time_label = tk.Label(bg_frame, bg="#ffffff", textvariable=remaining_time_display_msg)
+display_remaining_time_label.place(relx=0.5-0.35, rely=0.5, relwidth=0.7, relheight=0.2)
+
+refresh_button = tk.Button(bg_frame, bg="#fefecc", text="Refresh", command=refresh_time)
+refresh_button.place(relx=0.25, rely=0.85, relwidth=0.4, relheight=0.1)
 
 root.mainloop()
